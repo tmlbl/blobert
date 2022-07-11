aa1913d820073a0ecb6540cb31fd8b024b097f95
jmhuang <jm.observer@qq.com>
2022-07-12 00:11:58 +0800

    暂存

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
+use std::sync::Arc;
 // use oci_distribution::manifest;
-use actix_web::{web, App, HttpServer, HttpResponse, Responder};
 use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use env_logger::Env;
-use structopt::StructOpt;
 use log::error;
+use rustls::{Certificate, PrivateKey, ServerConfig};
+use rustls_pemfile::{certs, rsa_private_keys};
+use structopt::StructOpt;
 
-mod util;
-mod error;
 mod blob;
-mod upload;
+mod error;
 mod manifests;
 mod meta;
+mod upload;
+mod util;
 
 #[derive(StructOpt, Clone)]
 #[structopt(name = "blobert", about = "Another OCI registry")]
 pub struct Options {
-    #[structopt(long, default_value = "http")]
+    #[structopt(long, default_value = "https")]
     protocol: String,
 
-    #[structopt(short, long, default_value = "127.0.0.1")]
+    #[structopt(short, long, default_value = "www.duduwuli.cn")]
     host: String,
 
     #[structopt(short, long, default_value = "7000")]
@@ -54,7 +60,7 @@ impl Options {
 pub struct Blobert {
     pub opts: Options,
     pub meta_store: Box<dyn meta::Store>,
-    pub blob_store: blob::Store
+    pub blob_store: blob::Store,
 }
 
 impl Blobert {
@@ -65,12 +71,12 @@ impl Blobert {
         Blobert {
             opts,
             meta_store: Box::new(meta_store),
-            blob_store: blob::Store::new("/tmp/data", buf_size)
+            blob_store: blob::Store::new("/tmp/data", buf_size),
         }
     }
 
     async fn v2() -> impl Responder {
-        HttpResponse::Ok().body("true")
+        HttpResponse::Ok()
     }
 }
 
@@ -80,21 +86,80 @@ async fn main() -> std::io::Result<()> {
     let bind_addr = opts.get_bind_addr();
     env_logger::init_from_env(Env::default().default_filter_or(&opts.log_level));
 
+    let range_regex = web::Data::new(regex::Regex::new("^([0-9]+)-([0-9]+)$").unwrap());
+
     HttpServer::new(move || {
         App::new()
             .app_data(Blobert::new())
+            .app_data(range_regex.clone())
             .wrap(Logger::new("%r"))
             .route("/v2/", web::get().to(Blobert::v2))
-            .route("/v2/{namespace}/blobs/{id}", web::get().to(upload::get_blob))
-            .route("/v2/{namespace}/blobs/uploads/", web::post().to(upload::start_blob_upload))
-            .route("/v2/{namespace}/blobs/upload/{id}", web::patch().to(upload::patch_blob_data))
-            .route("/v2/{namespace}/blobs/upload/{id}", web::put().to(upload::put_blob_upload_complete))
-            .route("/v2/{namespace}/blobs/{digest}", web::head().to(upload::blob_exists))
-            .route("/v2/{namespace}/manifests/{reference}", web::put().to(manifests::put_manifest))
-            .route("/v2/{namespace}/manifests/{reference}", web::head().to(manifests::get_manifest))
-            .route("/v2/{namespace}/manifests/{reference}", web::get().to(manifests::get_manifest))
+            .route(
+                "/v2/{namespace}/blobs/{id}",
+                web::get().to(upload::get_blob),
+            )
+            .route(
+                "/v2/{namespace}/blobs/uploads/",
+                web::post().to(upload::start_blob_upload),
+            )
+            .route(
+                "/v2/{namespace}/blobs/upload/{id}",
+                web::patch().to(upload::patch_blob_data),
+            )
+            .route(
+                "/v2/{namespace}/blobs/upload/{id}",
+                web::put().to(upload::put_blob_upload_complete),
+            )
+            .route(
+                "/v2/{namespace}/blobs/{digest}",
+                web::head().to(upload::blob_exists),
+            )
+            .route(
+                "/v2/{namespace}/manifests/{reference}",
+                web::put().to(manifests::put_manifest),
+            )
+            .route(
+                "/v2/{namespace}/manifests/{reference}",
+                web::head().to(manifests::get_manifest),
+            )
+            .route(
+                "/v2/{namespace}/manifests/{reference}",
+                web::get().to(manifests::get_manifest),
+            )
     })
-    .bind(bind_addr)?
+    .bind_rustls(bind_addr, load_rustls_config())?
+    // .bind(bind_addr)?
     .run()
     .await
+}
+
+fn load_rustls_config() -> rustls::ServerConfig {
+    // init server config builder with safe defaults
+    let config = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth();
+
+    // load TLS key/cert files
+    let cert_file = &mut BufReader::new(File::open("www.duduwuli.cn_bundle.pem").unwrap());
+    let key_file = &mut BufReader::new(File::open("www.duduwuli.cn.key").unwrap());
+
+    // convert files to key/cert objects
+    let cert_chain = certs(cert_file)
+        .unwrap()
+        .into_iter()
+        .map(Certificate)
+        .collect();
+    let mut keys: Vec<PrivateKey> = rsa_private_keys(key_file)
+        .unwrap()
+        .into_iter()
+        .map(PrivateKey)
+        .collect();
+
+    // exit if no keys could be parsed
+    if keys.is_empty() {
+        eprintln!("Could not locate PKCS 8 private keys.");
+        std::process::exit(1);
+    }
+
+    config.with_single_cert(cert_chain, keys.remove(0)).unwrap()
 }
--- src/upload.rs
+++ src/upload.rs
@@ -1,13 +1,17 @@
 use actix_web::web;
-use actix_web::{Responder, HttpRequest, HttpResponse};
+use actix_web::{HttpRequest, HttpResponse, Responder};
 use futures::StreamExt;
-use uuid::Uuid;
 use log::{debug, error};
-use std::io::Write;
+use regex::Regex;
 use serde::Deserialize;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{Seek, SeekFrom, Write};
+use std::sync::Arc;
+use uuid::Uuid;
 
-use crate::Blobert;
 use crate::meta;
+use crate::Blobert;
 
 pub async fn get_blob(req: HttpRequest) -> impl Responder {
     let blobert: &Blobert = req.app_data().unwrap();
@@ -23,52 +27,75 @@ pub async fn get_blob(req: HttpRequest) -> impl Responder {
 }
 
 pub async fn start_blob_upload(req: HttpRequest) -> impl Responder {
+    debug!("start_blob_upload {:?}", req);
     let blobert: &Blobert = req.app_data().unwrap();
     let id = Uuid::new_v4();
     let namespace = req.match_info().get("namespace").unwrap();
-    let location = format!("{}/v2/{}/blobs/upload/{}", 
-            blobert.opts.get_server_url(), namespace, id);
-
+    let location = format!(
+        "{}/v2/{}/blobs/upload/{}",
+        blobert.opts.get_server_url(),
+        namespace,
+        id
+    );
     HttpResponse::Accepted()
         .append_header(("Location", location))
         .append_header(("Docker-Upload-UUID", id.to_string()))
         .finish()
 }
 
-pub async fn patch_blob_data(req: HttpRequest, mut payload: web::Payload) -> impl Responder {
+pub async fn patch_blob_data(
+    req: HttpRequest,
+    mut payload: web::Payload,
+    regex: web::Data<Regex>,
+) -> impl Responder {
+    debug!("patch_blob_data {:?}", req);
     let blobert: &Blobert = req.app_data().unwrap();
     let namespace = req.match_info().get("namespace").unwrap();
     let id = req.match_info().get("id").unwrap();
 
+    debug!("patch_blob_data id: {}", id);
     let mut blobfile = match blobert.blob_store.get_upload_file(id) {
         Ok(f) => f,
         Err(e) => {
             error!("Error getting upload file: {}", e);
-            return HttpResponse::InternalServerError().finish()
-        },
+            return HttpResponse::InternalServerError().finish();
+        }
     };
-
+    // let content_length = req.headers().get("content-length");
     let mut written: usize = 0;
+
+    if let Some(content_range) = req.headers().get("content-range") {
+        if let Ok(content_range) = content_range.to_str() {
+            let cap = regex.captures(content_range).unwrap();
+            let start: u64 = cap.get(1).unwrap().as_str().parse().unwrap();
+            let end: u64 = cap.get(2).unwrap().as_str().parse().unwrap();
+            debug!("{}-{}", start, end);
+            debug!("file len={}", blobfile.metadata().unwrap().len());
+            // blobfile.seek(SeekFrom::Start(start)).unwrap();
+        }
+    }
 
     while let Some(chunk) = payload.next().await {
         match chunk {
-            Ok(chunk) => {
-                match blobfile.write(&chunk) {
-                    Ok(size) => written += size,
-                    Err(e) => {
-                        error!("Error writing upload file: {}", e);
-                        return HttpResponse::InternalServerError().finish()
-                    }
+            Ok(chunk) => match blobfile.write(&chunk) {
+                Ok(size) => written += size,
+                Err(e) => {
+                    error!("Error writing upload file: {}", e);
+                    return HttpResponse::InternalServerError().finish();
                 }
             },
             Err(e) => {
                 error!("Error getting chunk: {}", e);
-                return HttpResponse::InternalServerError().finish()
+                return HttpResponse::InternalServerError().finish();
             }
         }
     }
-    let location = format!("{}/v2/{}/blobs/upload/{}", 
-            blobert.opts.get_server_url(), namespace, id);
+    let location = format!(
+        "{}/v2/{}/blobs/upload/{}",
+        blobert.opts.get_server_url(),
+        namespace,
+        id
+    );
     HttpResponse::Accepted()
         .append_header(("Location", location))
         .append_header(("Docker-Upload-UUID", id.to_string()))
@@ -79,18 +106,28 @@ pub async fn patch_blob_data(req: HttpRequest, mut payload: web::Payload) -> imp
 
 #[derive(Deserialize)]
 pub struct PutDigest {
-    digest: String
+    digest: String,
 }
 
-pub async fn put_blob_upload_complete(req: HttpRequest, info: web::Query<PutDigest>) -> impl Responder {
+pub async fn put_blob_upload_complete(
+    req: HttpRequest,
+    info: web::Query<PutDigest>,
+) -> impl Responder {
+    debug!("put_blob_upload_complete {:?}", req);
     let blobert: &Blobert = req.app_data().unwrap();
     let id = req.match_info().get("id").unwrap();
+    let namespace = req.match_info().get("namespace").unwrap();
 
+    // /v2/<name>/blobs/<digest>
     match blobert.blob_store.commit(id, &info.digest) {
-        Ok(_) => HttpResponse::Created()
-            .append_header(("Content-Length", "0"))
-            .append_header(("Docker-Content-Digest", info.digest.clone()))
-            .finish(),
+        Ok(_) => {
+            let location = format!("/v2/{}/blobs/upload/{}", namespace, info.digest);
+            HttpResponse::Created()
+                .append_header(("Location", location))
+                .append_header(("Content-Length", "0"))
+                .append_header(("Docker-Content-Digest", info.digest.clone()))
+                .finish()
+        }
         Err(e) => {
             error!("Error getting chunk: {}", e);
             HttpResponse::InternalServerError().finish()
@@ -105,6 +142,6 @@ pub async fn blob_exists(req: HttpRequest) -> impl Responder {
         true => HttpResponse::Ok()
             .append_header(("Docker-Content-Digest", digest))
             .finish(),
-        false => HttpResponse::NotFound().finish()
+        false => HttpResponse::NotFound().finish(),
     }
 }
